04a369eceaf533f6b4b4886e4ba319998f729504
Celeekeybor <celestinbor02@gmail.com>
2023-02-27 13:13:29 -0800

    size

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: String to get length 
+ * Return: length of @str
+ */
+size_t_strlen(const char *str)
+{
+	size_t length = 0;
+	while(*str++)
+	
+	while(*str++)
+	length++;
+	return (length);
+}
