9a6f8602bbd2ce21a459390a55c8ba484403ac0c
Celeekeybor <celestinbor02@gmail.com>
2023-02-27 15:28:58 -0800

    betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,20 +6,20 @@
  */
 void print_rev(char *s)
 {
-        int longi = 0;
-        int o;
+	int longi = 0;
+	int o;
 
-        while (*s != '\0')
-        {
-                longi++;
-                s++;
-        }
-        s--;
-        for (o = longi; o > 0; o--)
-        {
-                _putchar(*s);
-                s--;
-        }
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
 
-        _putchar('\n');
+	_putchar('\n');
 }
