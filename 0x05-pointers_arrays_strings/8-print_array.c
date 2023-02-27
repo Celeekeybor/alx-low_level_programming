b29be858a2fc2942068a2cfe372de664c9ad7499
Celeekeybor <celestinbor02@gmail.com>
2023-02-27 15:59:08 -0800

    celee

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_array -  print n elements of an array
+ * print_array - a function that prints n elements of an array
  * @a: array name
  * @n: is the number of elements OF the array to be printed
  * Return: a and n inputs
