2754d239e1abd056aa43618885084dba562aa284
Celeekeybor <celestinbor02@gmail.com>
2023-03-02 13:23:56 -0800

    always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase to uppercase 
+ * @str: the string to modify
+ *
+ * Return: a pointer to the modified string
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+{
+	if (*ptr >= 'a' && *ptr <= 'z')
+{
+	*ptr -= 32;
+}
+	ptr++;
+	}
+
+	return (str);
+}
