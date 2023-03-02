356ab77ef6ae12b487952038234589c77f28e442
Celeekeybor <celestinbor02@gmail.com>
2023-03-02 13:12:33 -0800

    paranoid

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,23 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strcmp - function that compares two strings
- * @s2: paramater for string 2
- * @s1: paramater for string 1
+ * main - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void print_array(int *a, int n)
+{
+    int i;
+
+    i = 0;
+    while (i < n)
+    {
+        if (i != 0)
+        {
+            printf(", ");
+        }
+        printf("%d", a[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code
+ *
  * Return: Always 0.
  */
-
-int _strcmp(char *s1, char *s2)
+int main(void)
 {
-	int k = 0;
+    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-	while (s1[k] != '\0')
-{
-	if (s1[k] < s2[k])
-	return (s1[k] - s2[k]);
-	if (s1[k] > s2[k])
-	return (s1[k] - s2[k]);
-	k++;
-}
-	return (0);
+    print_array(a, sizeof(a) / sizeof(int));
+    reverse_array(a, sizeof(a) / sizeof(int));
+    print_array(a, sizeof(a) / sizeof(int));
+    return (0);
 }
