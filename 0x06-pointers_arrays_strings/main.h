4a7b74100225106086cfa8073344ea0a8b67644f
Celeekeybor <celestinbor02@gmail.com>
2023-03-02 08:18:10 -0800

    celeeel

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int si;
+void print_buffer(char *b, int size;
 
 #endif
