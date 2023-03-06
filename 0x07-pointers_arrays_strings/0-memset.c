f3d970c732c42baff1b32c7434e8c22e03ba951c
Celeekeybor <celestinbor02@gmail.com>
2023-03-06 13:41:19 -0800

    celestine

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,28 +1,20 @@
 #include "main.h"
+
 /**
-* *_memset - a function that fills
-*         memory with a constant byte
-*
-* @s: input pointer to char type
-*    represents the pointer to the
-*    block of memory to fill
-* @b: input variable of char type
-*    represents the character to
-*    fill s
-* @n: unsigned int variable
-*    the number of bytes to be filled
-*
-* Return: A pointer to the filled memory
-*         area @s
-*/
+ * _memset - prints buffer in hexa
+ * @s: the address of memory to print
+ * @n: the size of the memory to print
+ * @b: the data type to print
+ * Return: Nothing.
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
+        while (n)
+        {
+                s[n - 1] = b;
+                n--;
+        }
+        return (s);
 }
