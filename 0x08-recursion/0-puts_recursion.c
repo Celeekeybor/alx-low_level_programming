fc4d1d9b1043a9c3c3547b67b5dd6c16bde31df3
Celeekeybor <celestinbor02@gmail.com>
2023-03-08 14:54:36 -0800

    this life

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
+ * _puts_recursion - function that prints a string
+ * @s: first argument
+ * Return: zero 
  */
+
 void _puts_recursion(char *s)
 {
-	for (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+        if (*s == '\0')
+        {
+                putchar('\n');
+                return;
+        }
+        putchar(*s);
+        _puts_recursion(s + 1);
 }
