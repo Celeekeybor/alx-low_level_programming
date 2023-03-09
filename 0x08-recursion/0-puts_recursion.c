b2c6907f86294f43aa2757d1cc116e3226100750
Celeekeybor <celestinbor02@gmail.com>
2023-03-08 16:22:43 -0800

    celeey

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
- */
+*  _puts_recursion -  prints a string, followed by a new line.
+* @s: string to print
+*/
+
 void _puts_recursion(char *s)
+
 {
 if (*s == '\0')
 {
@@ -12,5 +14,6 @@ _putchar('\n');
 return;
 }
 _putchar(*s);
-_puts_recursion(s + 1);
+s++;
+_puts_recursion(s);
 }
