0b0c6f4c346b83f00de14a4cd0c8df9a8eb849d0
Celeekeybor <celestinbor02@gmail.com>
2023-03-08 16:25:07 -0800

    life

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - function to print in reverse
+ * @s: last argument
+ * Return: always zero
+ */
+
+void _print_rev_recursion(char *s)
+{
+        if (*s == '\0')
+        {
+                return;
+        }
+        _print_rev_recursion(s + 1);
+        putchar(*s);
+}
