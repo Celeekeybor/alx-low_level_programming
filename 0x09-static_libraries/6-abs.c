c9022e4f05546fb27b2394d123db897860027464
Celeekeybor <celestinbor02@gmail.com>
2023-03-10 12:58:17 -0800

    hub

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Prints absolute value
+ * @n: Variable
+ * Return: Returns
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
