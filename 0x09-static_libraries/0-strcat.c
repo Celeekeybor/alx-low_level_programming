1daefae2d0608d8f7f24a99774a939db5f75977a
Celeekeybor <celestinbor02@gmail.com>
2023-03-10 17:13:03 -0800

    alxstatic

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * _strncat - function that concatenates two strings
- * @dest: parmater for dest to append to
- * @src: parameter for src
- * @n: parameter for n
+ * _strcat - function that concatenates two strings.
+ * @src: parameter to a src
+ * @dest: parameter to append
  * Return: Always 0.
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int a = 0;
 	int b = 0;
 
 	while (dest[a] != '\0')
 		a++;
-	while (src[b] != '\0' && b < n)
+	while (src[b] != '\0')
 	{
 		dest[a] = src[b];
 		b++;
