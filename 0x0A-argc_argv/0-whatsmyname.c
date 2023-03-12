6579b9bdb3ef356cdfbddf200c7715faa8c21bde
Celeekeybor <celestinbor02@gmail.com>
2023-03-11 23:28:51 -0800

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include "main.h"
 
+
+/**
+* main - function that print name of program
+* @argc: function of argument counter
+* @argv: function of array of arguments
+* Return: zero
+*/
+
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("argument of the program", argv[0]);
+
+return (0);
+}
