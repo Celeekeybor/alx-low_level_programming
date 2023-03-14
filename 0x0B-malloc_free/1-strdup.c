928356cd26cd9ddb036e0ed45b09fb009008b2b0
Celeekeybor <celestinbor02@gmail.com>
2023-03-14 15:51:45 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,20 +9,22 @@
  */
 char *_strdup(char *str)
 {
-unsigned int i, j;
-char *s;
-if (str == NULL)
-return (NULL);
-for (i = 0; str[i]; i++)		;
-i++;
-s = malloc(i * sizeof(char));
-if (s == NULL)
-{
-return (NULL);
-}
-for (j = 0; j < i; j++)
-{
-s[j] = str[j];
-}
-return (s);
+	unsigned int i, j;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		;
+	i++;
+	s = malloc(i * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+	{
+		s[j] = str[j];
+	}
+	return (s);
 }
