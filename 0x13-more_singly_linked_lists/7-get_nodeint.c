ac31fb2fbc30e18fd00ce3fffb1c3454a26d0c48
Celeekeybor <celestinbor02@gmail.com>
2023-04-04 09:01:46 -0700

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,7 +9,7 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	for( ; head != NULL && index > 0; head = head->next, --index)
+	for ( ; head != NULL && index > 0; head = head->next, --index)
 		;
 	return (head);
 }
