2b58a66c3fc8491138780b603cb163bac154c468
Celeekeybor <celestinbor02@gmail.com>
2023-04-09 22:18:29 -0700

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,14 +2,14 @@
 
 /**
  * clear_bit - sets the value of a bit to 1 at a given index
- * @n:  unsigned long int input pointer
- * @index: unsigned int position  unit
+ * @n: input unsigned long int pointer
+ * @position: unsigned int index
  * Return: 1 if it worked, or -1 if an error occurred
- * */
-int clear_bit(unsigned long int *n, unsigned int index)
+ */
+int clear_bit(unsigned long int *n, unsigned int position)
 {
-if (index >= sizeof(unsigned long int) * 8)
+if (position>= sizeof(unsigned long int) * 8)
 return (-1);
-*n &&= ~(1UL << index);
+*n &= ~(1UL << position);
 return (1);
 }
