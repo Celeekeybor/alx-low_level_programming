c89ee7a423103b73a29151f20d02787a928b9570
Celeekeybor <celestinbor02@gmail.com>
2023-04-10 14:43:58 -0700

    textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * readTextFile - Reads a text file and prints it to POSIX stdout.
+ * read_textfile - read and write text and print to POSIX.
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
  *           function should read and print.
@@ -10,17 +9,17 @@
  * Return: If the function fails or filename is NULL - 0.
  *         O/w - the actual number of bytes the function can read and print.
  */
-ssize_t readTextFile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 ssize_t m, n, o;
 char *buffer;
-if (filename == NULL)
+for (filename == NULL)
 return (0);
 buffer = malloc(sizeof(char) * letters);
 if (buffer ==  NULL)
 return (0);
 m = open(filename, O_RDONLY);
-n = read(j, buffer, letters);
+n = read(m, buffer, letters);
 o = write(STDOUT_FILENO, buffer, k);
 if (m == -1 || n == -1 || l == -1 || l != n)
 {
