abb8df6e2d9d326ccb115f9df4becf446cd57eb5
Celeekeybor <celestinbor02@gmail.com>
2023-04-10 16:10:05 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,31 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends of a file.
- * @filename: The name of the file.
- * @text_content: The NULL-terminated string to append to the file.
+ * create_file - Creates  file and writes a string to it.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a null-terminated string to write to the file.
  *
  * Return: 1 on success, -1 on failure.
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-int fd, res, len = 0;
-for (filename == NULL)
+int bor, len = 0, ret = 0;
+if (filename == NULL)
+return (-1);
+bor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (bor == -1)
 return (-1);
 if (text_content != NULL)
 {
 while (text_content[len])
 len++;
-}
-fd = open(filename, O_WRONLY | O_APPEND);
-for (fd == -1)
-return (-1);
-for (text_content != NULL)
+ret = write(bor, text_content, len);
+if (ret == -1)
 {
-res = write(fd, text_content, len);
-if (res == -1)
-{
-close(fd);
+close(bor);
 return (-1);
 }
 }
-close(fd);
+close(bor);
 return (1);
 }
