53d8566de58f0aab2d89dff94b5f6cd15be44232
Celeekeybor <celestinbor02@gmail.com>
2023-04-09 21:03:58 -0700

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,8 +8,7 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int num = 0;
-int converted_val;
+unsigned int converted_val = 0;
 int i;
 if (b == NULL)
 return (0);
@@ -17,7 +16,7 @@ for (i = 0; b[i] != '\0'; i++)
 {
 if (b[i] < '0' || b[i] > '1')
 return (0);
-num = (num << 1) + (b[i] - '0');
+converted_val = (converted_val << 1) + (b[i] - '0');
 }
-return (num);
+return (converted_val);
 }
