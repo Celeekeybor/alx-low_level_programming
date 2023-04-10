395d7d24d0fb872717a16c6cb7bb80edbbaec558
Celeekeybor <celestinbor02@gmail.com>
2023-04-09 23:50:26 -0700

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * flipped_count - Returns the number of bits  needed to be flipped
+ * bit_count - Returns the number of bits  needed to be flipped
  * @n: input unsigned long int pointer
  * @m: unsigned long int position of unit
  * Return: flipped_count
  */
-unsigned int flipped_count(unsigned long int n, unsigned long int m)
+unsigned int bit_count(unsigned long int n, unsigned long int m)
 {
 unsigned long int diff = n ^ m;
-unsigned int count = 0;
+unsigned int bit_count = 0;
 while (diff != 0)
 {
-flipped_count += diff & 1;
+bit_count += diff & 1;
 diff >>= 1;
 }
-return (flipped_count);
+return (bit_count);
 }
