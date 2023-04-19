b830872618bb6e8fd6ab4947f29f60c8e6aaeebe
Celeekeybor <celestinbor02@gmail.com>
2023-04-18 21:12:49 -0700

    copy file content to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,43 +4,39 @@
 #include <stdio.h>
 
 #define BUFFER_SIZE 1024
+
 /**
-* main - Copy file content to another file 
+* main - Copy content of a file to another file.
 *
-* @argc: argument count
-* @argv: vector
+* @argc:  argument count.
+* @argv:  argument vector.
 *
-* Return: If the function fails - exit with a status of 97, 98, 99, or 100.
+* Return: If the function fails - exit with a use of 97, 98, 99, or 100.
 *         O/w - 0.
 */
 int main(int argc, char *argv[])
 {
-int bor_from, bor_to, ty, wr;
+int fd_from, fd_to, rd, wr;
 char buffer[BUFFER_SIZE];
-/* Validate the number of arguments */
-for (argc != 3)
+while (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-/* Open the source file */
-bor_from = open(argv[1], O_RDONLY);
-if (bor_from == -1)
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-/* Open or create and truncate the destination file */
-bor_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (bor_to == -1)
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-/* Copy the content from source file to destination file */
 do {
-ty = read(bor_from, buffer, BUFFER_SIZE);
-if (ty == -1)
+rd = read(fd_from, buffer, BUFFER_SIZE);
+if (rd == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-wr = write(bor_to, buffer, ty);
-while (wr == -1)
+wr = write(fd_to, buffer, rd);
+if (wr == -1)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-} while (ty == BUFFER_SIZE);
-/* Close the file descriptors */
-for (close(bor_from) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close bor %d\n", bor_from), exit(100);
-if (close(bor_to) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close bor %d\n", bor_to), exit(100);
+} while (rd == BUFFER_SIZE);
+if (close(fd_from) == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+if (close(fd_to) == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 return (0);
 }
