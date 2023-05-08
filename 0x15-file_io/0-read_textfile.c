5160abd8ef740725d88b9e44ca1472fea25fc610
Celeekeybor <celestinbor02@gmail.com>
2023-05-08 11:36:48 -0700

    task zero

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,37 @@
 #include "main.h"
 
 /**
- * read_textfile - write and print to POSIx
- * @filename: elem 1
- * @letters: elem 2
- * Return: return -1
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: elemn1
+ * @letters: elemn2
+ *
+ * Return: zero
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int dayta, read_data_elem, write_elem_data;
-	char *buffer = malloc(sizeof(char) * letters);
+	int cey;
+	ssize_t wr, newFile;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	dayta = open(filename, O_RDWR);
-	if (dayta == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
-	read_data_elem = read(dayta, buffer, letters);
-	if (read_data_elem == -1)
+
+	newFile = open(filename, O_RDONLY);
+	cey = read(newFile, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, cey);
+	if (cey == -1 || newFile == -1 || wr == -1)
+	{
+		free(buffer);
 		return (0);
-	write_elem_data = write(STDOUT_FILENO, buffer, read_data_elem);
-	if (write_elem_data == -1)
-		return (0);
-	close(dayta);
+	}
+
 	free(buffer);
-	return (write_elem_data);
+	close(newFile);
+
+	return (wr);
+}
+
