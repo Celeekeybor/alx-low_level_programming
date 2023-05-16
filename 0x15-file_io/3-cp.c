1a915e9ec19f47cfe36272f5c3e375728a42c8f3
Celeekeybor <celestinbor02@gmail.com>
2023-05-16 03:57:02 -0700

    copy file contents

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,46 +1,44 @@
 #include "main.h"
 /**
- * main - function that copies contents of a file
- * @argc: Arg1
- * @argv: Arg2
- * Return: 0 
- */
+* main - copy file contents to another file
+* @argc: argument 1
+* @argv: Argument 2
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
-	int source, final, rd_bytes;
-	char buffer[BUFFER_SIZE];
-
-	if (argc != 3)
+int inf, end, size;
+char buffer[BUFFER_SIZE];
+if (argc != 3)
 {
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
 }
-source = open(argv[1], O_RDONLY);
-if (source == -1)
+inf = open(argv[1], O_RDONLY);
+if (inf == -1)
 {
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
-final = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664); /*rw-rw-r--*/
-
-while ((rd_bytes = read(source, buffer, BUFFER_SIZE)) > 0)
+end = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664); /*rw-rw-r--*/
+while ((size = read(inf, buffer, BUFFER_SIZE)) > 0)
 {
-	if (final == -1 || write(final, buffer, rd_bytes) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(source);
-		exit(99);
-	}
+if (end == -1 || write(end, buffer, size) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(inf);
+exit(99);
 }
-if (rd_bytes == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
 }
-if ((close(source)) == -1 || (close(final)) == -1)
+if (size == -1)
 {
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source);
-	exit(100);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+if ((close(inf)) == -1 || (close(end)) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", inf);
+exit(100);
 }
 return (0);
 }
