fca5d1906225c54ef45714041194e9d236370133
Celeekeybor <122092941+Celeekeybor@users.noreply.github.com>
2023-06-15 14:52:12 -0700

    get node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - finds the nth node of a doubly linked list
- * @head: pointer to head node
- * @index: node index to be found
+ * get_dnodeint_at_index - finds  node of a doubly linked list
+ * @head: head node
+ * @index: node index 
  *
- * Return: address of the new element
+ * Return: new element
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
