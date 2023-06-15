5369ff260373a46d74e696738e214f6a13f03934
Celeekeybor <122092941+Celeekeybor@users.noreply.github.com>
2023-06-15 13:14:35 -0700

    linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,23 +1,23 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- *print_dlistint - doubly linked list print
- * @h:argument
- * Return: counts
- */
-
-size_t print_dlistint(const dlistint_t *h)
-{
-	int num = 0;
-
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		num++;
-	}
-	return (num);
-}
+ #include <stdlib.h>
+  2 #include <string.h>
+  3 #include <stdio.h>
+  4 #include "lists.h"
+  5 
+  6 /**
+  7  *print_dlistint - doubly linked_list
+  8  * @h:element
+  9  * Return: true or false
+ 10  */
+ 11 
+ 12 size_t print_dlistint(const dlistint_t *h)
+ 13 {
+ 14         int value = 0;
+ 15 
+ 16         while (h != NULL)
+ 17         {
+ 18                 printf("%d\n", h->n);
+ 19                 h = h->next;
+ 20                 value++;
+ 21         }
+ 22         return (value);
+ 23 }
