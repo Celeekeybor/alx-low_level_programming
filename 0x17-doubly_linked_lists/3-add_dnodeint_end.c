ea024a9f6f16869b221a403a6a179febd9c31475
Celeekeybor <122092941+Celeekeybor@users.noreply.github.com>
2023-06-15 14:36:34 -0700

    add node to the end of  a list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,39 +1,39 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - adds a new node at the end of the doubly linked list
- * @head: pointer to head node
- * @n: int to be added
+ * add_dnodeint_end - adds a current node end of the doubly linked list
+ * @head: head node
+ * @n: node added
  *
- * Return: address of the new element
+ * Return: new element
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *temp;
+	dlistint_t *current, *celee;
 
 	if (head == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(dlistint_t));
+	current = malloc(sizeof(dlistint_t));
 
-	if (new == NULL)
+	if (current == NULL)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+	current->n = n;
+	current->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = new;
-		new->prev = NULL;
+		*head = current;
+		current->prev = NULL;
 	}
 	else
 	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
-		new->prev = temp;
+		celee = *head;
+		while (celee->next != NULL)
+			celee = celee->next;
+		celee->next = current;
+		current->prev = celee;
 	}
-	return (new);
+	return (current);
 }
