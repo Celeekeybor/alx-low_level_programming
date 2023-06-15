65a146d824f181a909586e2c0719fcb76e818c3f
Celeekeybor <celestinbor02@gmail.com>
2023-06-15 15:23:05 -0700

    sum the nodes

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,28 +1,19 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+
 /**
-* main - check the code
-*
-* Return: Always EXIT_SUCCESS.
-*/
-int main(void)
+ * sum_dlistint - prints sum of all data of a doubly linked list
+ * @head: pointer to head node
+ *
+ * Return: sum of all data
+ */
+int sum_dlistint(dlistint_t *head)
 {
-dlistint_t *head;
-int sum;
-head = NULL;
-add_dnodeint_end(&head, 0);
-add_dnodeint_end(&head, 1);
-add_dnodeint_end(&head, 2);
-add_dnodeint_end(&head, 3);
-add_dnodeint_end(&head, 4);
-add_dnodeint_end(&head, 98);
-add_dnodeint_end(&head, 402);
-add_dnodeint_end(&head, 1024);
-sum = sum_dlistint(head);
-printf("sum = %d\n", sum);
-free_dlistint(head);
-head = NULL;
-return (EXIT_SUCCESS);
+	size_t sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
