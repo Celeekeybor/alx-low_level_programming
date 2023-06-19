4743c9a7c73023188bb1222cb74680bcc1d0bccf
Celeekeybor <celestinbor02@gmail.com>
2023-06-19 12:03:58 -0700

    check if upper

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @elem: the character to be checked
+ * Return: true or false
+ */
+int _isupper(int elem)
+{
+	return (elem >= 'A' && elem <= 'Z');
+}
